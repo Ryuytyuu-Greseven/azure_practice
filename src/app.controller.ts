bd93835f42a132e94ea01dfc68eec9067a14505e
Ryuytyuu-Greseven <rukku93223@gmail.com>
2023-11-08 23:25:47 +0530

    Health check api added!

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('health_check')
+  serverStatus() {
+    return 'All is well Bava!';
+  }
 }
